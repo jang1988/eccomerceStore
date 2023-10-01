4f71c1f3a9a0624177caa7da788308bea6d17249
Baklay Petro <pustota1988@gmail.com>
2023-10-01 15:10:50 +0300

    fix

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,16 +1,15 @@
-import axios from "axios";
+import axios from 'axios';
 
-const BASE_URL = "https://strore-eccomerce.up.railway.app/api/";
+const BASE_URL = 'https://strore-eccomerce.up.railway.app/api/';
 
 const user = JSON.parse(localStorage.getItem('persist:root'))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
 
-
 export const publicRequest = axios.create({
-  baseURL: BASE_URL,
+    baseURL: BASE_URL,
 });
 
 export const userRequest = axios.create({
-  baseURL: BASE_URL,
-  headers: { token: `Bearer ${currentUser?.accessToken}` },
+    baseURL: BASE_URL,
+    headers: { token: `Bearer ${currentUser?.accessToken}` },
 });
